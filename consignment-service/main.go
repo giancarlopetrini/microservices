2f160d8fac6f749f3feac90bb26a2ad9d991c765
Giancarlo Petrini <giancarlo@libreworx.com>
2018-02-21 15:26:57 -0500

    added vessel service files & updated consignment interface name

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -8,27 +8,27 @@ import (
 	"golang.org/x/net/context"
 )
 
-// IRepository - interface for pushing Create
-type IRepository interface {
+// Repository - interface for pushing Create and GetAll
+type Repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
 	GetAll() []*pb.Consignment
 }
 
-// Repository - Dummy repository, this simulates the use of a datastore
+// ConsignmentRepository - Dummy repository, this simulates the use of a datastore
 // of some kind. We'll replace this with a real implementation later on.
-type Repository struct {
+type ConsignmentRepository struct {
 	consignments []*pb.Consignment
 }
 
 // Create - create consignment and append to repo
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
 	return consignment, nil
 }
 
 // GetAll - pull all consignments
-func (repo *Repository) GetAll() []*pb.Consignment {
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
 
@@ -37,7 +37,7 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 // in the generated code itself for the exact method signatures etc
 // to give you a better idea.
 type service struct {
-	repo IRepository
+	repo Repository
 }
 
 // CreateConsignment - we created just one method on our service,
@@ -66,7 +66,7 @@ func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *
 
 func main() {
 
-	repo := &Repository{}
+	repo := &ConsignmentRepository{}
 
 	srv := micro.NewService(
 		// This name must match the package name given in your protobuf definition
