eeb336f26007f143dcacbb9a0cdc9ba50d88a68c
Giancarlo Petrini <giancarlo@libreworx.com>
2018-02-21 12:52:50 -0500

    created build docker image and runtime container

---
+++ consignment-cli/cli.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	pb "github.com/giancarlopetrini/microservices/consignment-service/proto/consignment"
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/cmd"
+	"golang.org/x/net/context"
+)
+
+const (
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consigment *pb.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consigment)
+	return consigment, err
+}
+
+func main() {
+	cmd.Init()
+
+	client := pb.NewShippingServiceClient("go.micro.srv.consignment", microclient.DefaultClient)
+
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+	if err != nil {
+		log.Fatalf("Couldn't parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("Couldn't greet: %v", err)
+	}
+	log.Printf("Created: %t", r.Created)
+
+	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("could not get consignments: %v", err)
+	}
+	for _, c := range getAll.Consignments {
+		log.Println(c)
+	}
+
+}
---
+++ consignment-service/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/giancarlopetrini/microservices/consignment-service/proto/consignment"
+	micro "github.com/micro/go-micro"
+	"golang.org/x/net/context"
+)
+
+// IRepository - interface for pushing Create
+type IRepository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+// Repository - Dummy repository, this simulates the use of a datastore
+// of some kind. We'll replace this with a real implementation later on.
+type Repository struct {
+	consignments []*pb.Consignment
+}
+
+// Create - create consignment and append to repo
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	return consignment, nil
+}
+
+// GetAll - pull all consignments
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
+type service struct {
+	repo IRepository
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	// Save our consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
+	res.Created = true
+	res.Consignment = consignment
+	return nil
+}
+
+// GetConsignments - pulls consignments in
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
+	return nil
+}
+
+func main() {
+
+	repo := &Repository{}
+
+	srv := micro.NewService(
+		// This name must match the package name given in your protobuf definition
+		micro.Name("go.micro.srv.consignment"),
+		micro.Version("latest"),
+	)
+
+	// Init will parse the command line flags.
+	srv.Init()
+
+	// Register handler
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+
+	if err := srv.Run(); err != nil {
+		log.Fatalf("Coudn't run go-micro service: %v", err)
+	}
+}
